9857e5bc5e830b80d981d1b53ec69ad6ce79d5f2
+ <+>
2023-01-16 15:43:14 -0500

    Create a Function to find 31st March, 2023

--- countDown.js
+++ countDown.js
@@ -5,3 +5,6 @@ const deadline = new Date('3/31/2023 10:01 AM');
     const dHour = dMinute * 60;
     const dDay = dHour * 24;
     let timer;
+
+/* Function to find 31st March, 2023 */
+function showRemaining() {}
