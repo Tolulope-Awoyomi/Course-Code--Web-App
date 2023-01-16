e91153fa56a97714b3215d57be109c3829b0bd44
+ <+>
2023-01-16 15:44:50 -0500

    Inside of the function to find 31st march, include a code to subtract March 31st, 2023; 10:01 am from the current date

--- countDown.js
+++ countDown.js
@@ -7,4 +7,14 @@ const deadline = new Date('3/31/2023 10:01 AM');
     let timer;
 
 /* Function to find 31st March, 2023 */
-function showRemaining() {}
+function showRemaining() {
+    
+    /* Subtract March 31st, 2023; 10:01 am from the current date */
+    const currentDate = new Date();
+    const difference = deadline - currentDate;
+    if (difference < 0) {
+        clearInterval(timer);
+        document.getElementById('countDown').innerHTML = 'CLASSES HAVE BEGUN! KINDLY WAIT FOR OUR NEXT ANNOUCEMENT.';
+        return;
+    }
+}
