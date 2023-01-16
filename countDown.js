846f3a72de548cbc92f2574aeb293cc931a76999
+ <+>
2023-01-16 15:47:32 -0500

    calculate the days, hours, minutes, and seconds until 31st March, 2023

--- countDown.js
+++ countDown.js
@@ -8,7 +8,7 @@ const deadline = new Date('3/31/2023 10:01 AM');
 
 /* Function to find 31st March, 2023 */
 function showRemaining() {
-    
+
     /* Subtract March 31st, 2023; 10:01 am from the current date */
     const currentDate = new Date();
     const difference = deadline - currentDate;
@@ -17,4 +17,10 @@ function showRemaining() {
         document.getElementById('countDown').innerHTML = 'CLASSES HAVE BEGUN! KINDLY WAIT FOR OUR NEXT ANNOUCEMENT.';
         return;
     }
+
+    /* Calculate the days, hours, minutes, and seconds until 31st March, 2023 */
+    const days = Math.floor(difference / dDay);
+    const hours = Math.floor((difference % dDay) / dHour);
+    const minutes = Math.floor((difference % dHour) / dMinute);
+    const seconds = Math.floor((difference % dMinute) / dSecond);
 }
