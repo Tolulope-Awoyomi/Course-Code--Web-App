6773b940772753a7501b0d113f609d40f4450757
+ <+>
2023-01-16 15:42:12 -0500

    Create variables

---
+++ countDown.js
@@ -0,0 +1,7 @@
+const deadline = new Date('3/31/2023 10:01 AM');
+        
+    const dSecond = 1000;
+    const dMinute = dSecond * 60;
+    const dHour = dMinute * 60;
+    const dDay = dHour * 24;
+    let timer;
